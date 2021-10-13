7bfb67a791250977e07d96c96c96a6394214de7b
HaarikaR <hramadugu@gmail.com>
2021-10-13 15:26:53 -0700

    commit tree 12ac57883e03f23a44887928ed3a98b2213bc765

--- index.js
+++ index.js
@@ -1,13 +1,25 @@
 function isPalindrome(word) {
-  // Write your algorithm here
+
+  for (let i = 0; word.length - i > i; i++) {
+    if (word[i] !== word[word.length - 1 - i]) return false;
+  }
+  return true;
 }
+  
+
 
 /* 
   Add your pseudocode here
+  LOOP THRough half of the string
+  compare it with first string and last string at i are same
+  return if true or false
 */
 
 /*
   Add written explanation of your solution here
+
+  The solution here is to iterate tthrough the characters using a for loop.For each character at position i 
+  from the end.If any of these does not equal , then the condition returns false by breaking out of the for loop .
 */
 
 // You can run `node index.js` to view these console logs
